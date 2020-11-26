988cd778466c0f6843ea66bf204e637de18226ae
Harut Antonyan <harut@instigatemobile.com>
2020-11-26 20:10:39 +0400

    first commit

---
+++ run.py
@@ -0,0 +1,146 @@
+import argparse
+from datetime import datetime
+import os
+import json
+import smtplib
+import subprocess
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from multiprocessing import Pool
+from string import Template
+
+import requests
+
+from generate_html_report import GenerateHTMLReport
+from helpers import read_file
+
+parser = argparse.ArgumentParser(description='Typing run')
+parser.add_argument('--env', type=str, default='live', help='Set the environment in which tests will be run',
+                    choices=['new', 'live'])
+parser.add_argument('--page', '-p', nargs='+', help='run specific class tests', choices=['teacher-signup',
+                                                                                         'teacher-classes',
+                                                                                         'teacher-curriculum',
+                                                                                         'teacher-order',
+                                                                                         'teacher-assignments'])
+parser.add_argument('--throughput', help='simulate internet connection speed')
+parser.add_argument('--group', type=str, default='', help='run specific group tests')
+parser.add_argument('--case', type=str, default='', help='Input page')
+parser.add_argument('--thread_count', '-t', default=None, help=' run tests in parallel')
+parser.add_argument('--browser', '-b', nargs='+',
+                    help='specify the browser in which tests will be run, please note that you can specify multiple browsers',
+                    default=['chrome'],
+                    choices=['chrome', 'safari', 'firefox', 'edge'])
+parser.add_argument('--to_email', type=str, default=None, help='to send email report to specified email address')
+parser.add_argument('--from_email', type=str, default='harut@instigatemobile.com', help='specify sender email')
+parser.add_argument('--password', type=str, default='Mal!bu123', help='specify sender password')
+parser.add_argument('--smtp_host', type=str, default='mail.instigatemobile.com', help='specify SMTP host')
+parser.add_argument('--smtp_port', default='587', help='specify SMTP port')
+parser.add_argument('--send_slack', default=False)
+parser.add_argument('--slack_hook',
+                    default='https://hooks.slack.com/services/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',
+                    help='specify the Incoming Webhooks url')
+args = parser.parse_args()
+url_add = {"new": "new", "live": "www"}
+base_url = 'https://{}.typing.com'.format(url_add[args.env])
+os.environ["ENV"] = args.env
+os.environ["URL"] = base_url
+
+if args.throughput is not None:
+    os.environ['THROUGHPUT'] = args.throughput
+
+print(f'Running the tests using {args.browser} parallel threads')
+
+bashCommand = f"pytest -s --verbose " \
+              "--allure-link-pattern=test_case:https://docs.google.com/spreadsheets/d/1P54UmzfJggGuXzZV4zMfZ1I6Isv8P5Ac0a_F_u1Deyg/edit?ts=5fb694ba#gid=763370721&range={} --allure-link-pattern=issue:https://docs.google.com/spreadsheets/d/1P54UmzfJggGuXzZV4zMfZ1I6Isv8P5Ac0a_F_u1Deyg/edit?ts=5fb694ba#gid=886796583&range={}"
+if args.page is not None:
+    for page in args.page:
+        bashCommand += f" tests/{page}_page_test.py"
+else:
+    bashCommand += " tests/"
+if args.thread_count is not None:
+    bashCommand += f" -n {args.thread_count}"
+if args.group:
+    bashCommand += f' -m {args.group}'
+if args.case:
+    bashCommand = f' -k {args.case}'
+
+generate_report = GenerateHTMLReport()
+
+
+def send_slack_report(passed, failed, skipped, error, browser, platform, duration):
+    color = '#e51c23' if failed > 0 or error > 0 else '#259b24'
+    emoji = ':thumbsdown:' if failed > 0 or error > 0 else ':thumbsup:'
+    total = passed + failed + skipped + error
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    report = Template(read_file('templates/slack_report.json')).substitute(BROWSER=browser, DATE=now, EMOJI=emoji,
+                                                                           COLOR=color, PLATFORM=platform,
+                                                                           PASSED=passed, FAILED=failed,
+                                                                           SKIPPED=skipped, ERROR=error, TOTAL=total,
+                                                                           DURATION=duration)
+    json_params_encoded = json.dumps(json.loads(report))
+    requests.post(url=args.slack_hook, data=json_params_encoded, headers={"Content-type": "application/json"})
+
+
+def send_email_report(passed, fail, skip, error, browser, platform, duration):
+    status = 'FAILED' if fail > 0 or error > 0 else 'PASSED'
+    status_color = 'red' if status == 'FAILED' else 'green'
+    now = datetime.now()
+    total = passed + fail + skip + error
+    msg = MIMEMultipart('alternative')
+    to = args.to_email
+    me = args.from_email
+    pswd = args.password
+    msg['Subject'] = f"Test Automation Report | {passed}/{total} | {browser.upper()} |{now.strftime('%d/%m/%Y %H:%M:%S')}"
+    msg['From'] = me
+    msg['To'] = to
+    html = read_file("templates/email_report.html")
+    html = Template(html).substitute(PLATFORM=platform, ST_COLOR=status_color,
+                                     STATUS=status, BROWSER=browser.upper(), FAILED=fail, PASSED=passed,
+                                     SKIPPED=skip, ERROR=error, TOTAL=total, ENVIRONMENT=base_url,
+                                     URL=base_url, DURATION=duration)
+    part2 = MIMEText(html, "html")
+    msg.attach(part2)
+    s = smtplib.SMTP(args.smtp_host, args.smtp_port)
+    s.starttls()
+    s.login(me, pswd)
+    s.sendmail(me, [to], msg.as_string())
+
+
+def run_command(browser: str):
+    global bashCommand, reports
+    os.environ["BROWSER"] = browser
+    bashCommand += f' --alluredir=results_{browser} --json-report --json-report-summary --json-report-file={browser}_report.json'
+    print(bashCommand)
+    subprocess.call(bashCommand.split(' '))
+    subprocess.call(f"allure generate results_{browser} --clean -o reports/allure-reports_for_{browser}".split(' '))
+
+
+generate_report.append_title(args.browser)
+if __name__ == '__main__':
+    pool = Pool()
+    pool.map(run_command, args.browser)
+    pool.close()
+    pool.join()
+
+for browser in args.browser:
+    with open(f'{browser}_report.json') as json_report:
+        data = json.load(json_report)
+        duration = f"{round(data['duration'])} seconds"
+        platform = data['environment']['Platform']
+        summary = data['summary']
+        passed = summary['passed'] if 'passed' in summary else 0
+        failed = summary['failed'] if 'failed' in summary else 0
+        error = summary['error'] if 'error' in summary else 0
+    skipped = summary['skipped'] if 'skipped' in summary else 0
+    generate_report.append(browser, platform, duration, passed, failed, skipped, error)
+
+    if args.to_email is not None or args.send_slack:
+        if args.send_slack:
+            send_slack_report(passed, failed, skipped, error, browser, platform, duration)
+        if args.to_email is not None:
+            send_email_report(passed, failed, skipped, error, browser, platform, duration)
+
+generate_report.generate()
+
+print(f'1.Used {args.thread_count} threads to run this tests'
+      f'\n2.Used {args.browser} browsers fot this tests')
---
+++ tests/teacher-classes_page_test.py
@@ -0,0 +1,198 @@
+import time
+from random import randint
+
+import allure
+import pytest
+
+from apis.typing import delete_teacher_using_cache
+from pages.teacher_classes_page import SingleClassPage, TeacherClassesPage, DeleteClassDialog
+from pages.teacher_login_page import TeacherLoginPage
+
+
+@allure.feature("Teacher")
+@pytest.mark.usefixtures("get_driver")
+class TestTeacherClassesPage:
+
+    def setup(self):
+        self.login = TeacherLoginPage(self.driver)
+        self.login.get()
+        self.page = self.login.login()
+
+    @allure.testcase(
+        'A35:F35',
+        'CS_1')
+    @allure.title(
+        'With zero students in your class, click the "Add Students" button in the middle of the page. Add a single student')
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.story("Class Students")
+    @pytest.mark.class_students
+    def test_add_a_single_student(self):
+        username = f"tester{randint(10, 99)}test{randint(1000, 9999)}"
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_close) \
+            .click_on_class('Hello') \
+            .click_on_add_students_button() \
+            .create_a_new_student(username)
+        SingleClassPage(self.driver) \
+            .assert_students_tab_present() \
+            .assert_students_count_is(1) \
+            .assert_student_with_username_present(username)
+        delete_teacher_using_cache(self.driver)
+
+    @allure.testcase(
+        'A54:G54',
+        'CA_1')
+    @allure.title(
+        'From the assignments tab on the class page, click "Add Assignment" and create a new Lessons Assignment. Assign it to one class')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Class Assignments")
+    @allure.issue('13:13', "[Teacher] A long description gets out of the assignment summary window")
+    @pytest.mark.class_assignments
+    def test_add_a_new_lesson_assignment(self):
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_close) \
+            .click_on_class('Hello') \
+            .click_on_assignments_tab() \
+            .click_on_add_assignment_button() \
+            .create_typing_lessons_assignment()
+        SingleClassPage(self.driver) \
+            .assert_assignments_tab_present() \
+            .assert_assignments_tab_count_is(1)
+        delete_teacher_using_cache(self.driver)
+
+    @allure.testcase(
+        'A55:G55',
+        'CA_2')
+    @allure.title(
+        'From the assignments tab on the class page, click "Add Assignment" and create a new Written Prompt Assignment. Assign it to one class')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Class Assignments")
+    @allure.issue('13:13', "[Teacher] A long description gets out of the assignment summary window")
+    @pytest.mark.class_assignments
+    def test_add_a_written_prompt_assignment(self):
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_close) \
+            .click_on_class('Hello') \
+            .click_on_assignments_tab() \
+            .click_on_add_assignment_button() \
+            .create_written_prompt_assignment()
+        SingleClassPage(self.driver) \
+            .assert_assignments_tab_present() \
+            .assert_assignments_tab_count_is(1)
+        delete_teacher_using_cache(self.driver)
+
+    @allure.title(
+        'From the assignments tab on the class page, click "Add Assignment" and create a new Lessons Assignment. Assign it to multiple classes')
+    @allure.testcase('A57:G57', name='CA_4')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Class Assignments")
+    @allure.issue('13:13', "[Teacher] A long description gets out of the assignment summary window")
+    @pytest.mark.class_assignments
+    def test_add_a_lesson_assignment_for_multiple_classes(self):
+        self.page.create_a_new_class('Test1', self.page.create_a_new_class_close)
+        time.sleep(0.5)
+        self.page.create_a_new_class('Test2', self.page.create_a_new_class_close) \
+            .click_on_class('Test1') \
+            .click_on_assignments_tab() \
+            .click_on_add_assignment_button() \
+            .create_typing_lessons_assignment(classes="Test2", title='TestAssignment')
+        SingleClassPage(self.driver) \
+            .assert_my_classes_link_present() \
+            .assert_assignments_tab_present() \
+            .assert_assignments_tab_count_is(1) \
+            .assert_assignment_by_name_present('TestAssignment') \
+            .click_on_my_classes_link().click_on_class('Test2') \
+            .click_on_assignments_tab() \
+            .assert_assignments_tab_count_is(1) \
+            .assert_assignment_by_name_present('TestAssignment')
+
+    @allure.title(
+        'From the assignments tab on the class page, click "Add Assignment" and create a new Tests Assignment. Assign it to any number of students within your class')
+    @allure.testcase('A56:G56', name='CA_3')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Class Assignments")
+    @allure.issue('13:13', "[Teacher] A long description gets out of the assignment summary window")
+    @pytest.mark.class_assignments
+    def test_add_assignment_with_multiple_students(self):
+        username = f"tester{randint(10, 99)}test{randint(1000, 9999)}"
+        username1 = f"tester{randint(10, 99)}test{randint(1000, 9999)}"
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_close) \
+            .click_on_class('Hello') \
+            .click_on_add_students_button() \
+            .create_a_new_student((username, username1))
+        SingleClassPage(self.driver) \
+            .refresh_page() \
+            .click_on_assignments_tab() \
+            .click_on_add_assignment_button() \
+            .create_typing_assessment_assignment(title='TestAssignment', students=(username, username1),
+                                                 lessons="1 Page Typing Test")
+        SingleClassPage(self.driver) \
+            .assert_assignments_tab_present() \
+            .assert_assignments_tab_count_is(1) \
+            .assert_assignment_by_name_present('TestAssignment')
+
+    @allure.testcase(
+        'A19:F19',
+        'CLSS_1')
+    @allure.title(
+        'With zero classes, create a class using the "Create a new Class" button in the middle of the page. When asked to add students, say no')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Classes")
+    @pytest.mark.classes
+    def test_add_class_without_students(self):
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_see_my_classes) \
+            .assert_first_class_present() \
+            .assert_first_class_title('Hello')
+        delete_teacher_using_cache(self.driver)
+
+    @allure.testcase(
+        'A20:F20',
+        'CLSS_2')
+    @allure.title(
+        'With zero classes, create a class using the "Create a new Class" button in the middle of the page. When asked to add students, click "add students" and add a single student')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Classes")
+    @pytest.mark.classes
+    def test_add_class_add_students(self):
+        username = f"tester{randint(10, 99)}test{randint(1000, 9999)}"
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_add_students) \
+            .create_a_new_student(username)
+        TeacherClassesPage(self.driver)\
+            .click_on_class('Hello')
+        SingleClassPage(self.driver) \
+            .assert_students_tab_present() \
+            .assert_students_count_is(1) \
+            .assert_student_with_username_present(username)
+        delete_teacher_using_cache(self.driver)
+
+
+    @allure.testcase(
+        'A21:F21',
+        'CLSS_3')
+    @allure.title(
+        'Select a class with students and attempt to delete it using the "bulk actions" delete option')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Classes")
+    @pytest.mark.classes
+    def test_delete_class_with_students(self):
+        username = f"tester{randint(10, 99)}test{randint(1000, 9999)}"
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_add_students) \
+            .create_a_new_student(username)
+        TeacherClassesPage(self.driver)\
+            .assert_bulk_actions_present().select_class_with_name('Hello').select_delete_from_bulk_actions()\
+            .assert_delete_class_error_present().assert_delete_class_error_contains("You can not delete classes which contain students")
+
+
+
+    @allure.testcase(
+        'A22:F22',
+        'CLSS_4')
+    @allure.title(
+        'Select 1 or more classes that do not have students and delete them using the "bulk actions" delete option')
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.story("Classes")
+    @pytest.mark.classes
+    def test_delete_class_without_students(self):
+        self.page.create_a_new_class('Hello', self.page.create_a_new_class_see_my_classes)
+        TeacherClassesPage(self.driver)\
+            .assert_bulk_actions_present().select_class_with_name('Hello').select_delete_from_bulk_actions()
+        DeleteClassDialog(self.driver)\
+        .assert_confirm_text_field_present()\
+        .confirm().assert_class_missing('Hello')
